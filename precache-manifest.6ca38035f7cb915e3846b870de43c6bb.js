026f6710c5ad5854aa8980bce8d0c1071f0213eb
kd <0xbeefdead@gmail.com>
2019-08-01 16:32:30 +0530

    Updates

---
+++ precache-manifest.6ca38035f7cb915e3846b870de43c6bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9034ee58674b9586633d86099b86bb35",
+    "url": "/camera-webapp/index.html"
+  },
+  {
+    "revision": "37189f372f862dbf5c22",
+    "url": "/camera-webapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a720ec823dc4eec53643",
+    "url": "/camera-webapp/static/js/2.4f28fee0.chunk.js"
+  },
+  {
+    "revision": "37189f372f862dbf5c22",
+    "url": "/camera-webapp/static/js/main.c8813d90.chunk.js"
+  },
+  {
+    "revision": "163bca17d64e9a68391d",
+    "url": "/camera-webapp/static/js/runtime~main.cb8ced1e.js"
+  }
+]);
