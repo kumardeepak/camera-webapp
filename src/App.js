13c79ce734abe224430643e288aff62ef7a01339
kd <0xbeefdead@gmail.com>
2019-08-01 14:02:42 +0530

    face detection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-// import { Route, Router } from 'react-router-dom';
-// import createHistory from 'history/createBrowserHistory';
 import './App.css';
 
-// import Home from './views/Home';
-// import ImageInput from './views/ImageInput';
 import VideoInput from './views/VideoInput';
 
 class App extends Component {
