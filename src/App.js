6fd52029e83fcaf6478383c51039af2347bd4fc6
kd <0xbeefdead@gmail.com>
2019-08-01 16:28:23 +0530

    removed packages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import VideoInput from './views/VideoInput';
-import { TextDecoder,TextEncoder } from 'text-encoding';
+// import { TextDecoder,TextEncoder } from 'text-encoding';
 
-if (!window['TextDecoder']) {
-  window['TextDecoder'] = TextDecoder;
-}
-if (!window['TextEncoder']) {
-  window['TextEncoder'] = TextEncoder;
-}
+// if (!window['TextDecoder']) {
+//   window['TextDecoder'] = TextDecoder;
+// }
+// if (!window['TextEncoder']) {
+//   window['TextEncoder'] = TextEncoder;
+// }
 
 class App extends Component {
   render() {
